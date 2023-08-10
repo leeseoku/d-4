7121f107ccafd3e3fab7f24cb62d3819c05bd5d6
leeseoku <leeseoku@naver.com>
2023-08-10 16:18:23 +0900

    슬라이드탑배너

--- js/test.js
+++ js/test.js
@@ -1,4 +1,48 @@
-$(document).ready(function(){let count=0; setInterval(function(){count++; count %=$(".banner div").length; $(".banner div").eq(count).addClass("op").siblings().removeClass("op")},3000);
+$(document).ready(function(){
+   
+   
+   
+   
+   
+   
+   
+    let hh =$(".slides div").height();
+    let dir =-1
+
+
+
+
+setInterval(function(){
+    $(".slides").animate({"marginTop":hh*dir},9000,function(){
+        $(".slides div").eq(0).appendTo(".slides");
+        $(".slides").css({"marginTop": 0}) })
+        ,3000})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 $("#gnb").on('mouseenter', function(){$(".banner span").removeClass("opn")}).on('mouseleave', function(){$(".banner span").addClass("opn")});
     $(".content2 li:nth-child(1) li:nth-child(1)").click(function(){
        $(".popup").removeClass("d-none");
